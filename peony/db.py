ceb783dbe23d405cf0a3ca3421bbd26869c6a44d
Vytautas Jancauskas <unaudio@gmail.com>
2022-03-25 09:59:18 +0100

    updates to the basic db structure and tests'

--- peony/db.py
+++ peony/db.py
@@ -1,12 +1,22 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.sql import select, func
 from geoalchemy2 import Geometry
 
 Base = declarative_base()
+
+def load_spatialite(dbapi_conn, connection_record):
+    dbapi_conn.enable_load_extension(True)
+    dbapi_conn.load_extension('mod_spatialite')
+
+def init_spatial_metadata(engine):
+    conn = engine.connect()
+    conn.execute(select([func.InitSpatialMetaData()]))
+    conn.close()
 
 class Image(Base):
     __tablename__ = 'image'
     id = Column(Integer, primary_key=True)
     path = Column(String)
-    geom = Column(Geometry('POLYGON'))
+    geom = Column(Geometry('POLYGON', management=True))
     date = Column(Date)
--- test/test_db.py
+++ test/test_db.py
@@ -1,3 +1,34 @@
 import pytest
-from peony.db import Image
+from peony.db import Image, load_spatialite, init_spatial_metadata
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.event import listen
+import datetime
 
+def test_db():
+    engine = create_engine('sqlite://', echo=True)
+    listen(engine, 'connect', load_spatialite)
+    init_spatial_metadata(engine)
+    Image.__table__.create(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(Image(path='/datastore/sentinel/1',
+                      geom='POLYGON((0 0, 0.5 0, 0.5 0.5, 0 0.5, 0 0))',
+                      date=datetime.date(2019, 1, 1)))
+    session.add(Image(path='/datastore/sentinel/2',
+                      geom='POLYGON((0.5 0, 1.0 0, 1.0 0.5, 0.5 0.5, 0.5 0))',
+                      date=datetime.date(2020, 1, 1)))
+    session.add(Image(path='/datastore/sentinel/3',
+                      geom='POLYGON((0 0.5, 0.5 0.5, 0.5 1.0, 0 1.0, 0 0.5))',
+                      date=datetime.date(2021, 1, 1)))
+    session.add(Image(path='/datastore/sentinel/4',
+                      geom='POLYGON((0.5 0.5, 1.0 0.5, 1.0 1.0, 0.5 1.0, 0.5 0.5))',
+                      date=datetime.date(2022, 1, 1)))
+    session.commit()
+    query = session.query(Image).filter(
+        Image.geom.ST_Overlaps('POLYGON((0.1 0.1, 0.9 0.1, 0.9 0.4, 0.1 0.4, 0.1 0.1))'))
+    for image in query:
+        print(image)
+
+    
+    
