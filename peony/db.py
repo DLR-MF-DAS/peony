2f2429711a6ccca1929ec615053d913514fdf9eb
Vytautas Jancauskas <unaudio@gmail.com>
2022-03-30 14:49:55 +0200

    added csv_2_spatialite

--- peony/db.py
+++ peony/db.py
@@ -1,9 +1,60 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.event import listen
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.sql import select, func
 from geoalchemy2 import Geometry
+import datetime
+import pathlib
 
 Base = declarative_base()
+
+class Image(Base):
+    """A class representing a single piece of satellite data.
+    """
+    __tablename__ = 'image'
+    id = Column(Integer, primary_key=True)
+    path = Column(String)
+    name = Column(String)
+    geom = Column(Geometry('POLYGON', management=True))
+    date = Column(Date)
+
+def csv_2_spatialite(csv_path, sqlite_path):
+    """Populates a spatialite database based on a CSV file.
+
+    Parameters
+    ----------
+    csv_path: str
+        A path to a CSV file with no header and 4 columns.
+        The columns are in this order: date, polygon coordinates, 
+        name and path.
+    sqlite_path: str
+        Path to where a spatialite database will be stored.
+    """
+    engine = create_engine(f"sqlite:///{sqlite_path}")
+    listen(engine, 'connect', load_spatialite)
+    init_spatial_metadata(engine)
+    Image.__table__.create(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    counter = 0
+    with open(csv_path, 'r') as fd:
+        for line in fd:
+            line = line.strip().split(',')
+            date_str = line[0].strip('"').split('.')[0]
+            date = datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
+            polygon = line[1].strip('"').strip().split(' ')
+            assert(len(polygon) % 2 == 0)
+            polygon = ', '.join([polygon[i] + ' ' + polygon[i + 1] for i in range(len(polygon) // 2)])
+            polygon = f"POLYGON(({polygon}))"
+            name = line[2].strip('"').strip()
+            path = pathlib.PurePath(line[3].strip('"').strip()).parent
+            session.add(Image(path=str(path), geom=polygon, name=name, date=date))
+            counter += 1
+            if (counter % 1000) == 0:
+                session.commit()
+    session.commit()
 
 def load_spatialite(dbapi_conn, connection_record):
     dbapi_conn.enable_load_extension(True)
@@ -13,10 +64,3 @@ def init_spatial_metadata(engine):
     conn = engine.connect()
     conn.execute(select([func.InitSpatialMetaData()]))
     conn.close()
-
-class Image(Base):
-    __tablename__ = 'image'
-    id = Column(Integer, primary_key=True)
-    path = Column(String)
-    geom = Column(Geometry('POLYGON', management=True))
-    date = Column(Date)
