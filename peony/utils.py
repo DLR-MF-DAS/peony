62b758d89e59d8712f558a949eea180dc7283631
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2022-09-08 10:26:23 +0000

    Update peony/utils.py

--- peony/utils.py
+++ peony/utils.py
@@ -2,7 +2,7 @@ import json
 from geoalchemy2 import WKTElement
 from pyreproj import Transformer
 
-def geojson_to_wktelement(jsonfile, to_crs='epsg:3857'):
+def geojson_to_wktelement(jsonfile, to_srs='epsg:3857'):
     """Extracts the first polygon from a GeoJSON file.
     
     Parameters
@@ -17,8 +17,9 @@ def geojson_to_wktelement(jsonfile, to_crs='epsg:3857'):
     with open(jsonfile, 'r') as fd:
         data = json.load(fd)
         coordinates = data["features"][0]["geometry"]["coordinates"][0]
-    transformer = Transformer.from_crs('epsg:4326', to_crs)
-    coordinates = [transformer.transform(point[0], point[1]) for point in coordinates]
+    rp = Reprojector()
+    transform = rp.get_transformation_function(from_srs='epsg:4326', to_srs=to_srs)
+    coordinates = [transform(point[0], point[1]) for point in coordinates]
     coordinates = [(point[0] / 10000, point[1] / 10000) for point in coordinates]
     polygon = ", ".join([f"{point[0]} {point[1]}" for point in coordinates])
     return WKTElement(f"Polygon(({polygon}))")
