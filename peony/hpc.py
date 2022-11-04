d2c002e54532ad1c7343879bc8c3b5f8111135f1
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2022-11-04 12:14:14 +0000

    Update peony/hpc.py

--- peony/hpc.py
+++ peony/hpc.py
@@ -1,4 +1,7 @@
 from peony.db import query_polygon
+import itertools
+import tempfile
+import numpy as np
 from joblib import Parallel, delayed
 from pypyr import pipelinerunner
 import logging
@@ -24,6 +27,29 @@ def pipeline_on_polygon(workdir, pipeline, sqlite_path, polygon, date_range=None
     n_jobs = int(n_jobs)
     Parallel(n_jobs=n_jobs)(delayed(run_pipeline)(entry.path, entry.date, entry.name) for entry in entries)
 
-def pipeline_on_uniform_grid(workdir, pipeline, grid_size, rectangle=[[-180, -180], [180, -180], [180, 180], [-180, 180]]):
-    pass
-
+def pipeline_on_uniform_grid(workdir, pipeline, grid_size, longitude_range=(-180, 180), latitude_range=(-90, 90), n_jobs=48):
+    assert(longitude_range[0] < longitude_range[1])
+    assert(latitude_range[0] < latitude_range[1])
+    nx = int((longitude_range[1] - longitude_range[0]) / grid_size)
+    ny = int((latitude_range[1] - latitude_range[0]) / grid_size)
+    xs = np.linspace(longitude_range[0], longitude_range[1], nx)
+    ys = np.linspace(latitude_range[0], latitude_range[1], ny)
+    def run_pipeline(i, j):
+        rectangle = {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "coordinates": [[(xs[i], ys[j]), (xs[i + 1], ys[j]), (xs[i + 1], ys[j + 1]), (xs[i], ys[j + 1]), (xs[i], ys[j])]],
+                "type": "Polygon"
+                }
+            }]
+        }
+        subworkdir = os.path.join(workdir, f"{i}_{j}")
+        os.makedirs(subworkdir, exist_ok=True)
+        fd, filename = tempfile.mkstemp('.json', dir=workdir, text=True)
+        json.dump(rectangle, fd)
+        fd.close()
+        pipelinerunner.run(pipeline_name=pipeline, args_in=[f"geojson={filename}", f"workdir={subworkdir}", f"logfile={workdir}/logfile.log"])
+    Parallel(delayed(run_pipeline)(i, j) for i, j in itertools.product(range(nx - 1), range(nx - 1), n_jobs=n_jobs)
