2b2ac64417a84e08a5ab55da2ebea475184a3753
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2022-11-04 12:29:31 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1dev',
     packages=['peony'],
     license='GPLv3',
-    scripts=['bin/peony'],
+    scripts=['bin/peony', 'bin/peony_gee_download'],
     long_description=open('README.md').read()
 )
