c49c0543d79b0638785cce8c94290e83e8a524b7
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2022-11-23 12:36:22 +0000

    Update pipelines/dummy.yaml, test/test_hpc.py
    Former-commit-id: f2c1f86eb09ae4b42514c27da1a697cdd842926d

--- test/test_hpc.py
+++ test/test_hpc.py
@@ -2,4 +2,4 @@ import pytest
 from peony.hpc import pipeline_on_uniform_grid
 
 def test_pipeline_on_uniform_grid(tmp_path):
-    pipeline_on_uniform_grid(tmp_path, 'examples.dummy', 5)
+    pipeline_on_uniform_grid(tmp_path, 'dummy', 5)
