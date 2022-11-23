35660dd423dcd25e57cc876a475428029c314e3d
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2022-11-23 13:31:27 +0000

    Update peony/hpc.py
    Former-commit-id: f15410e24231b6d00703b28027e4ae95d96c4f49

--- peony/hpc.py
+++ peony/hpc.py
@@ -54,4 +54,4 @@ def pipeline_on_uniform_grid(workdir, pipeline, grid_size, longitude_range=(-180
         json.dump(rectangle, fd)
         fd.close()
         pipelinerunner.run(pipeline_name=pipeline, args_in=[f"geojson={filename}", f"workdir={subworkdir}", f"logfile={workdir}/logfile.log"])
-    Parallel(n_jobs=n_jobs)(delayed(run_pipeline)(i, j) for i, j in itertools.product(range(nx - 2), range(nx - 2)))
+    Parallel(n_jobs=n_jobs)(delayed(run_pipeline)(i, j) for i, j in itertools.product(range(nx - 1), range(ny - 1)))
