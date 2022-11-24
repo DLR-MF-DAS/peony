75fe29cc50634f60fb6da2217c0d2899f77c6b7e
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2022-11-24 14:26:57 +0000

    Update examples/gee_europe.py
    Former-commit-id: e5e5e5141e4f6ba1483901d9eb317731f390b813

---
+++ examples/gee_europe.py
@@ -0,0 +1,4 @@
+from peony.hpc import pipeline_on_uniform_grid
+
+if __name__ == '__main__':
+    pipeline_on_uniform_grid("/workdir/gee_europe", "gee_lcz", 1.0, longitude_range=[-27.0, 45], latitude_range=[35.0, 65.0], n_jobs=5)
