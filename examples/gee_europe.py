95059ffc839aee4a4798c516b98d2faf91b7e573
Vytautas Jancauskas <ge83noc2@cm2login4.cos.lrz.de>
2022-11-25 12:10:09 +0000

    updates
    Former-commit-id: c9de5bf47777c957feea39994401f706a4788fbd

--- examples/gee_europe.py
+++ examples/gee_europe.py
@@ -1,4 +1,4 @@
 from peony.hpc import pipeline_on_uniform_grid
 
 if __name__ == '__main__':
-    pipeline_on_uniform_grid("/workdir/gee_europe", "gee_lcz", 1.0, longitude_range=[-27.0, 45], latitude_range=[35.0, 65.0], n_jobs=5)
+    pipeline_on_uniform_grid("/workdir/gee_europe", "gee_lcz", 0.5, longitude_range=[-27.0, 45], latitude_range=[35.0, 65.0], n_jobs=5)
