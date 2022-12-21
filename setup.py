f2da72c16b3dea7c43d9c425587f260b3c85a268
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2022-12-21 14:59:18 +0000

    Update setup.py
    Former-commit-id: 6cf68e6caf26ca973bb89317d8e4f953fdb315de

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1dev',
     packages=['peony'],
     license='GPLv3',
-    scripts=['bin/peony', 'bin/peony_gee_download'],
+    scripts=['bin/peony', 'bin/peony_gee_download', 'bin/peony_success_matrix'],
     long_description=open('README.md').read()
 )
