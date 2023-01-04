e20bad8b9d7ae0f272deed769790ea33494d9e7e
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-01-04 15:28:38 +0000

    Update peony/db.py
    Former-commit-id: 4173345da1624b53bde4bb5dfdc70eb3d94237b9

--- peony/db.py
+++ peony/db.py
@@ -129,6 +129,7 @@ def download_gee_composite(geojson_path, output_path, collection='COPERNICUS/S2_
         medoid_task = medoid_im.export(medoid_asset_id, type='asset', region=polygon, crs='EPSG:4326', scale=10, dtype='uint16', wait=True)
         medoid_asset_im = gd.MaskedImage.from_id(medoid_asset_id)
         medoid_asset_im.download(output_path, region=polygon, scale=10, overwrite=True)
+        ee.data.deleteAsset(medoid_asset_id)
     else:
         coll = gd.MaskedCollection.from_name(collection)
         coll = coll.search(start_date=start_date, end_date=end_date, region=polygon, cloudless_portion=cloudless_portion)
