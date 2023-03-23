4a8b2b0d502e140a49ab08f1893b41c22d3af92e
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-03-23 13:39:23 +0000

    Update test/test_lcz.py
    Former-commit-id: 794499d99463f23006fca9e8f5eaa526a9ae9f77

--- test/test_lcz.py
+++ test/test_lcz.py
@@ -9,15 +9,15 @@ from peony.lcz import inferenceData
 
 def test_inference(tmp_path):
     inferenceData("test/0_0.tif", "test/s2_lcz_weights.hdf5", tmp_path)
-    with rasterio.open("0_0_lab_ref.tif") as src:
+    with rasterio.open("test/0_0_lab_ref.tif") as src:
         lab_ref = src.read()
-    with rasterio.open("0_0_pro_ref.tif") as src:
+    with rasterio.open("test/0_0_pro_ref.tif") as src:
         pro_ref = src.read()  
     assert os.path.exists(os.path.join(tmp_path, "0_0_lab.tif"))
     assert os.path.exists(os.path.join(tmp_path, "0_0_pro.tif"))
-    with rasterio.open("0_0_lab.tif") as src:
+    with rasterio.open(os.path.join(tmp_path, "0_0_lab.tif")) as src:
         lab = src.read()
-    with rasterio.open("0_0_pro.tif") as src:
+    with rasterio.open(os.path.join(tmp_path, "0_0_pro.tif")) as src:
         pro = src.read()
     assert np.all(lab_ref == lab)
     assert np.all(pro_ref == pro)
