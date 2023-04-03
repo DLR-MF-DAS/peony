4eabdae4128e241b715742360ca480aab3aa4ece
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-04-03 11:16:33 +0000

    Update test/test_inference.py
    Former-commit-id: dc30fd4de161fc9f42e172b6d68181a1b3d9e621

--- test/test_inference.py
+++ test/test_inference.py
@@ -1,4 +1,4 @@
 from peony.inference import bayesian_inference_on_geotiff
 
 def test_bayesian_inference(tmp_path):
-    bayesian_inference_on_geotiff("test/Lumberton_ROI_pro.tif", "test/Lumberton_ROI_ESA_WorldCover.tif", tmp_path)
+    bayesian_inference_on_geotiff("test/Lumberton_ROI_pro.tif", "test/Lumberton_ROI_ESA_WorldCover", tmp_path)
