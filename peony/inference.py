2e46eb0c88ca63e167759ce70e952f8083d6581d
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-04-03 08:32:15 +0000

    Update peony/inference.py
    Former-commit-id: 1423737fbfdae95959a393aa2182a832aac1f1ee

--- peony/inference.py
+++ peony/inference.py
@@ -1,2 +1,18 @@
-def bayesian_chain(hypothesis, evidence, translation):
+def bayesian_inference(hypothesis, evidence, likelihood):
+    """Apply Bayesian inference given a hypothesis (prior) and evidence (transformed into likelihood).
+
+    Parameters
+    ----------
+    hypothesis: NumPy array
+    evidence: NumPy array
+    likelihood: function
+    
+    Returns
+    -------
+    NumPy array
+        an array with the posterior distribution.
+    """
+    return likelihood(evidence) * hypothesis
+
+def bayesian_inference_on_geotiff(hypothesis, evidence, likelihood):
     pass
