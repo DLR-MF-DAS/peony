e103c6df02251939468713adc2e6dd8a46724038
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-04-17 11:37:41 +0000

    Update peony/utils.py
    Former-commit-id: e716192870f35363a707e7def48c0b1da9f5568a

--- peony/utils.py
+++ peony/utils.py
@@ -18,3 +18,9 @@ def geojson_to_wktelement(jsonfile, to_srs='epsg:3857'):
         coordinates = data["features"][0]["geometry"]["coordinates"][0]
     polygon = ", ".join([f"{point[1]} {point[0]}" for point in coordinates])
     return WKTElement(f"Polygon(({polygon}))")
+
+def probability_to_classes(pro_geotiff, lab_geotiff):
+    """Reads a geotiff with probability distribution and write a corresponding label file.
+    Using max likelihood.
+    """
+    pass
