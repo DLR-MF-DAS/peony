b87bb14edc7e9fd52ffc090be45cb50ede38da61
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-04-20 13:02:27 +0000

    Update test/test_inference.py
    Former-commit-id: c342d698832080fe5ec3d5c231dbc1effac55464

--- test/test_inference.py
+++ test/test_inference.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from peony.inference import bayesian_inference_on_geotiff
+from peony.utils import probability_to_classes
 import rasterio
 
 # ESA WorldCover classes
@@ -62,3 +63,4 @@ def test_bayesian_inference(tmp_path):
     with rasterio.open(os.path.join(tmp_path, 'test.tif')) as src:
         data = src.read()
     assert(np.isclose(data.sum(axis=0), 10000, rtol=0, atol=5).all())
+    probability_to_classes(os.path.join(tmp_path, 'test.tif'), os.path.join(tmp_path, 'test_lab.tif'))
