56dd40df67cdf97cb68461cdb5da5212c33a15e4
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-04-21 11:31:59 +0000

    Update peony/utils.py
    Former-commit-id: 48305834ab7dd5109a52b46a47c013cbd0582890

--- peony/utils.py
+++ peony/utils.py
@@ -21,14 +21,14 @@ def geojson_to_wktelement(jsonfile, to_srs='epsg:3857'):
     polygon = ", ".join([f"{point[1]} {point[0]}" for point in coordinates])
     return WKTElement(f"Polygon(({polygon}))")
 
-def probability_to_classes(pro_geotiff, lab_geotiff):
+def probability_to_classes(pro_geotiff, lab_geotiff, index_to_label=lambda x: x + 1):
     """Reads a geotiff with probability distribution and write a corresponding label file.
     Using max likelihood.
     """
     with rasterio.open(pro_geotiff) as src:
         pro = src.read()
         profile = src.profile
-    lab = np.argmax(pro, axis=0)
+    lab = index_to_label(np.argmax(pro, axis=0))
     profile.update(dtype=rasterio.uint8, count=1, compress='lzw')
     with rasterio.open(lab_geotiff, 'w', **profile) as dst:
         dst.write(lab, 1)
