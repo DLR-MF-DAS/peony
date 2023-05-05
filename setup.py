f3b9a1c5bb3340462441a483ea3015d39c88d100
Jancauskas, Vytautas <vytautas.jancauskas@dlr.de>
2023-05-05 09:53:36 +0000

    Update setup.py
    Former-commit-id: df3ce04da46326ddea0e255991ac3f88bcad1cf1

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1dev',
     packages=['peony'],
     license='GPLv3',
-    scripts=['bin/peony', 'bin/peony_gee_download', 'bin/peony_success_matrix', 'bin/peony_lcz_inference', 'bin/peony_run_grid_pipeline', 'bin/peony_lcz_from_geojson', 'bin/peony_gee_download_arbitrary', 'bin/peony_bayesian_inference', 'bin/peony_pro_to_lab'],
+    scripts=['bin/peony', 'bin/peony_gee_download', 'bin/peony_success_matrix', 'bin/peony_lcz_inference', 'bin/peony_run_grid_pipeline', 'bin/peony_lcz_from_geojson', 'bin/peony_gee_download_arbitrary', 'bin/peony_bayesian_inference', 'bin/peony_pro_to_lab', 'bin/peony_crop_from_tiff'],
     long_description=open('README.md').read()
 )
