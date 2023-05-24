4fd3020d87e9002f898294442dbc136883d9c76c
Vytautas Jancauskas <ge83noc2@cm2login1.cos.lrz.de>
2023-05-24 13:19:38 +0000

    added the ability to leave likelihood specification empty
    Former-commit-id: b8c18c444d722ffb6c3e120080447db6e2793783

--- peony/inference.py
+++ peony/inference.py
@@ -3,14 +3,18 @@ from rasterio.enums import Resampling
 import numpy as np
 from peony.utils import resample_2d
 
-def bayesian_inference_on_geotiff(hypothesis_path, evidence_path, posterior_path, likelihood=lambda x, y: x, prob_scale=10000):
+def bayesian_inference_on_geotiff(hypothesis_path, evidence_path, posterior_path, likelihood=lambda x, y: x, prob_scale=10000, band=0):
     with rasterio.open(hypothesis_path) as h_src:
         hypothesis = h_src.read()
         profile = h_src.profile
         with rasterio.open(evidence_path) as e_src:
             evidence = e_src.read()
-            evidence = resample_2d(evidence[0], h_src.height, h_src.width)
-    assert hypothesis.shape[1:] == evidence.shape, f"hypothesis shape {hypothesis.shape} is not the same as evidence shape {evidence.shape}"
+            if band is not None:
+                evidence = resample_2d(evidence[band], h_src.height, h_src.width)
+            else:
+                for band in range(evidence.shape[0]):
+                    evidence[band] = resample_2d(evidence[band], h_src.height, h_src.width)
+    #assert hypothesis.shape[1:] == evidence.shape, f"hypothesis shape {hypothesis.shape} is not the same as evidence shape {evidence.shape}"
     posterior = likelihood(evidence, hypothesis) * hypothesis
     posterior = posterior / posterior.sum(axis=0).astype(float)
     assert np.isclose(np.nan_to_num(posterior.sum(axis=0), nan=1.0), 1.0).all()
